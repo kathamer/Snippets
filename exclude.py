b45cbece663e165fe13d74fb8d7a9ac59e1266a1
Dylan Hamer <dylanhamer13@gmail.com>
2018-05-17 13:00:04 +0100

    Why don't I test my code

--- exclude.py
+++ exclude.py
@@ -7,7 +7,8 @@ By Dylan Hamer
 def exclude(originalList, exclusionIndex):
     """Return a given list without a specified element"""
     newList = originalList[:]  # Make original list not mutable
-    if exclusionIndex <= len(newList)  # Check if list has item
-        return newList.pop(exclusion)  # Return list but without exclusion
+    if exclusionIndex <= len(newList): # Check if list has item
+        newList.pop(exclusionIndex)  # Remove item at specific index
+        return newList
     else:  # If it doesn't have the item
         raise IndexError("Could not exclude {}".format(exclusion))  # Raise an error condition
