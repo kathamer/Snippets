40d2e3bd314ca618d2dd1c4498d22a5f7d401272
Dylan Hamer <dylanhamer13@gmail.com>
2018-05-17 13:19:41 +0100

    Update calculatetimetaken.py

--- calculatetimetaken.py
+++ calculatetimetaken.py
@@ -4,10 +4,11 @@ Calculate minutes, seconds and hours between two epoch points
 By Dylan Hamer
 """
 
+import time  # Deal with time
+
 def calculateTimeTaken(startTime, endTime):
     """Calculate difference between two epoch points in human readable time"""
-    epochTaken = endTime - startTime
-    timeStructure = time.localtime(epochTaken)
-    hours, minutes, seconds = timeStructure.tm_hour, timeStructure.tm_min, timeStructure.tm_sec
-    print("{} {} {}".format(hours,minutes,seconds))
+    epochTaken = endTime - startTime  # Get raw difference between two floating points
+    timeStructure = time.localtime(epochTaken)  # Structure a time constructor using localtime
+    hours, minutes, seconds = timeStructure.tm_hour, timeStructure.tm_min, timeStructure.tm_sec  # Get hours, minutes and seconds
     return hours, minutes, seconds
