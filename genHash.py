79411ce469142ce40a1d6b1b9e8e8b9530b01b50
Dylan Hamer <dylanhamer13@gmail.com>
2018-05-25 12:37:46 +0100

    Update genHash.py

--- genHash.py
+++ genHash.py
@@ -3,6 +3,12 @@ GenerateHash
 Generate a sha256 hash
 By Dylan Hamer
 """
+
 import hashlib  # Hashing library
 
 def generateHash(data):
+    encodedData = data.encode()  # Encode data as raw binary data
+    hashObject = hashlib.sha256(encodedData)  # Generate a hashlib hash object of the data
+    hashText = hashObject.hexdigest()  # Get hash data as raw text
+    return hashText
+  
